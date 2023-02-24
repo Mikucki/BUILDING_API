94eb02168fff85ddcb3abd19f26a589441475374
Piotr Mikucki <100493144+Mikucki@users.noreply.github.com>
2023-02-24 17:03:00 +0100

    tessss

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,37 +1,38 @@
+const asyncHendler = require("express-async-handler");
 //@ desc GET goals
 //@ route GET /api/goals
 //@acces Private
 
-const getGoals = (req, res) => {
+const getGoals = asyncHendler(async (req, res) => {
   res.status(200).json({ message: "get goal" });
-};
+});
 
 //@ desc put goals
 //@ route put /api/goals/:ID
 //@acces Private
 
-const putGoals = (req, res) => {
+const putGoals = asyncHendler(async (req, res) => {
   res.status(200).json({ message: `update goal nr ${req.params.id}` });
-};
+});
 
 //@ desc POST goals
 //@ route POST /api/goals
 //@acces Private
 
-const postGoals = (req, res) => {
+const postGoals = asyncHendler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("pease add a trext field");
   }
   res.status(200).json({ message: "set goal" });
-};
+});
 
 //@ desc Delete goals
 //@ route delete /api/goals/:ID
 //@acces Private
 
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHendler(async (req, res) => {
   res.status(200).json({ message: `deleted goal nr ${req.params.id}` });
-};
+});
 
 module.exports = { getGoals, putGoals, postGoals, deleteGoal };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const goals = require("./routes/goalRoutes");
+const errorHandler = require("./middleware/errorMiddleware.js");
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goals);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`server started on port ${port}`);
